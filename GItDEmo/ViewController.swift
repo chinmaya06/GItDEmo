ec1ba2cfba8ca60fce357d3ed605b80dcc87a02a
chinmaya <pchinmaya854@gmail.com>
2025-05-17 21:13:56 +0530

    test

--- GItDEmo/ViewController.swift
+++ GItDEmo/ViewController.swift
@@ -11,9 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        Abc()
         // Do any additional setup after loading the view.
     }
-
+    func Abc (){
+        print("chinmaya pradhan")
+    }
 
 }
 
