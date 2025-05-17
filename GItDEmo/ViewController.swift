82dee6fa2576fa1c4e29515cbec90eefe66a6e46
chinmaya <pchinmaya854@gmail.com>
2025-05-17 19:26:50 +0530

    Initial Commit

---
+++ GItDEmo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GItDEmo
+//
+//  Created by IOTtech software on 2025/5/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
